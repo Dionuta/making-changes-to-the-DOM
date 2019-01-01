2ad6831c57fb682a330fb02eae25c4ad88bead38
Dionuta <diounta@gmail.com>
2019-01-01 14:59:09 -0800

    Paased

--- 3-making-changes-to-the-DOM/vid-3-styling-elements/start/app.js
+++ 3-making-changes-to-the-DOM/vid-3-styling-elements/start/app.js
@@ -1,6 +1,20 @@
-const input = document.querySelector('input');
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+const input = document.querySelector('input.description');
 const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+const button = document.querySelector('button.description');
+
+//hides everything inside the list div
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none'){
+  toggleList.textContent ='Hide List';
+  listDiv.style.display = 'block';
+  }else{
+    toggleList.textContent ='Show List';
+  listDiv.style.display = 'none';
+}
+});
+
 
 button.addEventListener('click', () => {
   p.innerHTML = input.value + ':';
